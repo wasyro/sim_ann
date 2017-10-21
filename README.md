e3e857ec37b75e3336d82b11fb466cad87cdc58c
wasyro <sdookdrp@gmail.com>
2017-10-21 19:07:15 +0900

    implement optimizing by simulated annealing

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # sim_ann
-Solve TSP(Japanese Prefectures) using simulated annealing
+Solve TSP(Japanese Prefectures) using simulated annealing  
+
+# Environment
+Python 3.6.2  
+Anaconda3 - 5.0.0  
+
+# How To
+'lonlat_to_xy.py' converts longtitude and latitude to x-y coordinates.  
+input file needs to be shaped;  
+(longtitude0) (latitude0)  
+(longtitude1) (latitude1)  
+(longtitude2) (latitude2)  
+.  
+.  
+.  
+lontitude0 and latitude0 are coordinates of the original point in an x-y plane.  
+
--- anneal.py
+++ anneal.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import time
+import threading
 
 def input_coordinates(showmap=False):
     with open('prefs.out', 'r') as fin:
@@ -19,7 +21,6 @@ def input_coordinates(showmap=False):
         plt.scatter(X, Y)
         plt.xlim([-5000, 2000000])
         plt.ylim([-5000, 2000000])
-        plt.show()
 
     return X, Y
 
@@ -31,6 +32,52 @@ def coordinates_to_distance_table(coordinates):
             distance_list.append(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
         distance_table.append(distance_list)
     return distance_table
+
+def calc_distance(path, X, Y):
+    distance = 0
+    i = 0
+    while i < path.size - 1:
+        present_idx = path[i]
+        next_idx = path[i + 1]
+        distance += math.sqrt((X[present_idx] - X[next_idx]) ** 2
+                              + (Y[present_idx] + Y[next_idx]) ** 2)
+        i += 1
+    return distance
+
+def prob_exec(prob):
+    if np.random.rand() <= prob:
+        return True
+    else:
+        return False
+
+def random_path():
+    path = np.random.permutation(47)
+    path = np.append(path, path[0])
+    return path
+
+def metropolis(path1, X, Y, distance1, T):
+    path2 = np.copy(path1)
+    n = path1.size
+    swap_cities_idx = np.random.randint(1, n-1, size=2)
+    while True:
+        if swap_cities_idx[0] == swap_cities_idx[1]:
+            swap_cities_idx = np.random.randint(1, n-1, size=2)
+        else:
+            break
+
+    path2[swap_cities_idx[0]], path2[swap_cities_idx[1]] = \
+            path2[swap_cities_idx[1]], path2[swap_cities_idx[0]]
+
+    distance2 = calc_distance(path2, X, Y)
+    if distance1 < distance2:
+        delta = distance2 - distance1
+        prob = math.exp(- delta / T)
+        if prob_exec(prob):
+            return path2, distance2
+        else:
+            return path1, distance1
+    else:
+        return path2, distance2
 
 def greedy_tsp(distance_table):
     distance_table = np.array(distance_table)
@@ -45,8 +92,8 @@ def greedy_tsp(distance_table):
         bin_path[path[i]] = False
         distance_list = distance_table[city]
         if (bin_path == False).all():
-            nearest_city = path[0]
-            path = np.append(path, nearest_city)
+            initial_city = path[0]
+            path = np.append(path, initial_city)
         else:
             nearest_distance = np.min(distance_list[np.argwhere(bin_path)])
             nearest_city = int(np.argwhere(distance_list == nearest_distance))
@@ -54,24 +101,56 @@ def greedy_tsp(distance_table):
         city = nearest_city
     return path
 
-def showmap(X, Y, path):
+def anneal(path, X, Y, exec_time=10, T=100, step=1000,
+           annealing_schedule=0.995, figure=False):
+    path = np.copy(path)
+    distance = calc_distance(path, X, Y)
+    distances = []
+    time_list = []
+    time1, time2 = time.time(), time.time()
+    while time2 - time1 < exec_time:
+        for _ in range(step):
+            path, distance = metropolis(path, X, Y, distance, T)
+        time_list.append(time2 - time1)
+        distances.append(distance)
+        T *= annealing_schedule
+        time2 = time.time()
+
+    if figure:
+        plt.plot(time_list, distances, color='dimgray', lw=1)
+    return path
+
+def showmap(path, X, Y):
     path_sorted_X = []
     path_sorted_Y = []
     for city_num in path:
         path_sorted_X.append(X[city_num])
         path_sorted_Y.append(Y[city_num])
-    fig, ax = plt.subplots()
-    plt.xlim([-5000, 2000000])
-    plt.ylim([-5000, 2000000])
-    ax.tick_params(labelbottom='off', bottom='off',
-                   labelleft='off', left='off')
-    plt.gca().set_aspect('equal', adjustable='box')
-    ax.plot(path_sorted_X, path_sorted_Y, marker='o', markersize=8, color='black')
-    plt.show()
+    plt.xticks([])
+    plt.yticks([])
+    plt.plot(path_sorted_X, path_sorted_Y, marker='o',
+            markersize=5, color='dimgray', lw=1)
 
 if __name__ == '__main__':
-    X, Y = input_coordinates(showmap=False)
+    X, Y = input_coordinates()
     coordinates = list(zip(X, Y))
     distance_table = coordinates_to_distance_table(coordinates)
     path = greedy_tsp(distance_table)
-    showmap(X, Y, path)
+
+    plt.subplot(121)
+    plt.title('Annealing result')
+    plt.xlabel('CPU Time')
+    plt.ylabel('Tour length (m)')
+
+    path = anneal(path, X, Y, exec_time=60, T=1000000, step=1000,
+                  annealing_schedule=0.995, figure=True)
+    plt.subplot(122)
+    plt.title('Route')
+    showmap(path, X, Y)
+    plt.show()
+
+    with open('result', 'w') as fout:
+        fout.write('path\n')
+        for city in path:
+            fout.write(str(city) + '\n')
+        fout.write('')
